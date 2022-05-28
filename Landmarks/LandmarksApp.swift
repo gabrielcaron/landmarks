da75ab6c71d71f7420b91c77c893556c802850db
Gabriel Caron <jadgukkuqBi9vimzof>
2022-05-28 14:43:42 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Gabriel Caron on 2022-05-28.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
