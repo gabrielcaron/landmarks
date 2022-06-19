367a18774d0fce5b77626e3a1a580a3782f4539a
Gabriel Caron <gabriel.caron@gmail.com>
2022-06-19 15:15:32 -0400

    Building Lists and Navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Gabriel Caron on 2022-05-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
